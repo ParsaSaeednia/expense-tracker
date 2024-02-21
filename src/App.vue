a855c0fac1aadf3f3db8358c81fbaa653ebceffa
ParsaSaeednia <p.parsa31@gmail.com>
2024-02-21 19:27:59 +0330

    test

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,26 @@ import ExpenseBalance from './components/ExpenseBalance.vue';
 import ExpenseHeader from './components/ExpenseHeader.vue';
 import ExpenseData from './components/ExpenseData.vue';
 import ExpenseForm from './components/ExpenseForm.vue';
+import { onMounted, ref } from 'vue';
+import axios from 'axios'
 
+async function getUsers() {
+  const users = await axios.get('https://reqres.in/api/users')
+  const usersData = users.data.data;
+  console.log(usersData);
+  usersRef.value = usersData
+}
+onMounted(() => getUsers())
+
+const usersRef = ref(null)
 </script>
 
 <template>
   <ExpenseHeader title="Expense Tracker" />
+  <div v-for="item in usersRef" :key="item.id">
+    {{ item.email }} |
+    {{ item.first_name }}
+  </div>
   <ExpenseBalance />
   <ExpenseData />
   <ExpenseForm />
